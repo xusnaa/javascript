5d37545e0b5bf0adc10fe0a9b97c040c78590d9a
xusnaa <xusnaya@gmail.com>
2025-04-23 16:51:19 -0500

    style2

--- Todolist/index.js
+++ Todolist/index.js
@@ -11,32 +11,35 @@ function formsubmit(event) {
 
   if (tasklist.length === 0) {
     console.log("Please fill out at least one field.");
-  } else {
-    const ul = document.getElementById("todo-list");
-
-    const li = document.createElement("li");
-    const buttonDone = document.createElement("button");
-    const buttonDelete = document.createElement("button");
-    buttonDone.textContent = "Done";
-    buttonDelete.textContent = "Delete";
-
-    li.textContent = tasklist.join("  ");
-    li.appendChild(buttonDone);
-    li.appendChild(buttonDelete);
-    ul.appendChild(li);
-
-    buttonDone.onclick = () => {
-      const ul2 = document.getElementById("completed");
-      const completedli = document.createElement("li");
-      completedli.textContent = tasklist.join("");
-      ul2.appendChild(completedli);
-      li.remove();
-    };
-    buttonDelete.onclick = () => {
-      li.remove();
-    };
-    document.querySelector("form").reset();
+    return;
   }
+  const ul = document.getElementById("todo-list");
+
+  const li = document.createElement("li");
+
+  const span = document.createElement("span");
+  span.textContent = tasklist.join(" ");
+  const buttonDone = document.createElement("button");
+  const buttonDelete = document.createElement("button");
+  buttonDone.textContent = "Done";
+  buttonDelete.textContent = "Delete";
+
+  li.appendChild(span);
+  li.appendChild(buttonDone);
+  li.appendChild(buttonDelete);
+  ul.appendChild(li);
+
+  buttonDone.onclick = () => {
+    const ul2 = document.getElementById("completed");
+    const completedli = document.createElement("li");
+    completedli.textContent = tasklist.join("");
+    ul2.appendChild(completedli);
+    li.remove();
+  };
+  buttonDelete.onclick = () => {
+    li.remove();
+  };
+  document.querySelector("form").reset();
 }
 
 function showTaskForm() {
