98f783c186a04f235f6e6d83d343857a36008ad5
xusnaa <xusnaya@gmail.com>
2025-05-03 18:15:21 -0500

    foreshadow

--- Weather/index.js
+++ Weather/index.js
@@ -1,5 +1,6 @@
 let debounceTimer;
 const rapidKey = "3a5241315amsh478d4394adde39bp1c53b8jsn69df6c75e7ed";
+const apikey = "f8455688a12beff3eb0cdb594f057bcd";
 const citysearch = (event) => {
   clearTimeout(debounceTimer);
   debounceTimer = setTimeout(() => {
@@ -23,22 +24,54 @@ const citysearch = (event) => {
           li.textContent = `${city.name}, ${city.countryCode}`;
 
           li.onclick = () => getWeather(city.latitude, city.longitude);
+
           ul.appendChild(li);
         });
       }, 400);
   });
 };
 
-const apikey = "8ca54f8f7c7dc0c429249622fb161dd9";
 function getWeather(lat, lon) {
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}`;
   fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      const weatherDescription = document.getElementById("tempreature");
-      weatherDescription.textContent = `<div>${data.main.temp} ${data.weather[0].description}</div>`;
-      //   document.body.innerHTML = `<h2>${data.name}</h2><p>Humidity : ${data.weather[0].description}</p> <p>lon : ${data.coord.lon}</p> <p>lat : ${data.coord.lat}</p>`;
+
+      const weatherDescription = document.getElementById("description");
+      weatherDescription.innerHTML = `
+      <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Weather icon" />
+      <div>${data.main.temp} °K - ${data.weather[0].description}</div>
+      
+    `;
+      getForecast(data.coord.lat, data.coord.lon);
     });
 }
+
+function getForecast(lat, lon) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&exclude=minutely,hourly,current,alerts&units=metric&appid=${apikey}`;
+  console.log("Fetching forecast:", url); // Debug line
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const forecast = document.getElementById("forecast");
+      forecast.innerHTML = "";
+
+      data.daily.slice(0, 7).forEach((day) => {
+        const date = new Date(day.dt * 1000).toDateString();
+        const icon = day.weather[0].icon;
+        forecast.innerHTML += `
+          <div>
+            <h4>${date}</h4>
+            <img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="${day.weather[0].description}">
+            <p>${day.weather[0].description}</p>
+            <p>Day Temp: ${day.temp.day} °C</p>
+          </div>
+        `;
+      });
+    });
+}
+
 document.getElementById("city").addEventListener("input", citysearch);
